7749f2f9d518c57ddc4b35c3c99d74d963842eca
Deadwalk <zgdmemail@gmail.com>
2025-06-06 08:53:29 +0800

    性能优化：去掉mlflow容器安装工具的逻辑，加速docker镜像打包过程

--- docker/mlflow/start.sh
+++ docker/mlflow/start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-# 安装必要的工具
-apt-get update && apt-get install -y curl && rm -rf /var/lib/apt/lists/*
-
 # 创建必要的目录
 mkdir -p /app/mlflow_data/mlruns /app/mlflow_data/mlartifacts
 chmod -R 755 /app/mlflow_data
